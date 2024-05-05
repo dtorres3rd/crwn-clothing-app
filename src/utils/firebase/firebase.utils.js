c1d6c74dd659bca56295993ea51942d72a5e0d12
daniel.torres <daniel.torres3rd@gmail.com>
2024-05-05 18:30:04 +0800

    added code for firebase sign in with Google Redirect as reference.

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -17,14 +17,15 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
     prompt: "select_account"
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
@@ -33,7 +34,7 @@ export const createUserDocumentFromAuth =  async (userAuth) => {
     
     const userSnapshot = await getDoc(userDocRef);
  
-    //check if document exist from firestore db
+    //check if document-user exist from firestore db
     if(!userSnapshot.exists()){
         const { displayName, email } = userAuth;
         const createdAt = new Date();
