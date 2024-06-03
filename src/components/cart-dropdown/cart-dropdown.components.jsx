b1a02cdcfbfcf864686a0e35e3a3cf012dc12288
daniel.torres <daniel.torres3rd@gmail.com>
2024-06-03 20:28:31 +0800

    chore: implemeted styled components for cart-dropdown and tried out nested - targeted styling

--- src/components/cart-dropdown/cart-dropdown.components.jsx
+++ src/components/cart-dropdown/cart-dropdown.components.jsx
@@ -6,25 +6,30 @@ import { CartContext } from '../../contexts/cart.context';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
-import './cart-dropdown.styles.scss'
+import { CartDropDownContainer, EmptyMessage, CartItems } from './cart-dropdown.styles.jsx'
 
-const CartDropDown = () =>{
+const CartDropDown = () => {
     const { cartItems } = useContext(CartContext);
     const navigate = useNavigate();
 
-    const goToCheckoutHandler = () =>{
+    const goToCheckoutHandler = () => {
         navigate('/checkout');
     }
 
     return (
-        <div className='cart-dropdown-container'> 
-            <div className='cart-items'>
-                {cartItems.map((item) => (
-                    <CartItem key={item.id} cartItem={item} />
-                ))}
-            </div>
+        <CartDropDownContainer>
+            <CartItems>
+                {
+                    /* if cartItems has length, proceed to map function */
+                    cartItems.length ? (cartItems.map((item) => (
+                        <CartItem key={item.id} cartItem={item} />
+                    ))) : (
+                        <EmptyMessage>Your cart is empty</EmptyMessage>
+                    )
+                }
+            </CartItems>
             <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-        </div>
+        </CartDropDownContainer>
     )
 };
 
