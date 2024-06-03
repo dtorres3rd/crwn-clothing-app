16a3dd43828942b15af8f81b27a9106b60164c09
daniel.torres <daniel.torres3rd@gmail.com>
2024-06-03 20:04:44 +0800

    chore: tried out styled components for buttons.

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,17 +1,29 @@
-import './button.styles.scss'
+import { BaseButton, GoogleSignInButton, InvertedButton } from './button.styles.jsx'
 
 
 export const BUTTON_TYPE_CLASSES = {
+    base: 'base',
     google: 'google-sign-in',
     inverted: 'inverted'
 }
+// create helper function for button
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => (
+    /* map the values received from props button */
+    {
+        [BUTTON_TYPE_CLASSES.base]: BaseButton,
+        [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
+        [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
 
+    }[buttonType]
+);
+ 
 const Button = ({ children, buttonType, ...otherProps }) => {
+    const CustomButton = getButton(buttonType);
     return (
         // implement string interpolation for button type classes
-        <button className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`} {...otherProps} >
-         {children}
-        </button>
+        <CustomButton {...otherProps}>
+            {children}
+        </CustomButton>
     )
 };
 
